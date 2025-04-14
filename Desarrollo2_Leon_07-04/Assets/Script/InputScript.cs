7351636522baba7fb26289b06f1b652567ffc061
Nicolas Leon <nicolasaleon05@gmail.com>
2025-04-14 00:06:33 -0300

    Hold jump added

--- Desarrollo2_Leon_07-04/Assets/Script/InputScript.cs
+++ Desarrollo2_Leon_07-04/Assets/Script/InputScript.cs
@@ -7,46 +7,51 @@ public class InputScript : MonoBehaviour
     [SerializeField] private InputActionReference moveAction;
     [SerializeField] private InputActionReference jumpAction;
     [SerializeField] private GameObject player;
+    [SerializeField] private Rigidbody rigidBody;
 
-    [SerializeField] Rigidbody rigidBody;
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float holdJumpForce = 8f;
+    [SerializeField] private float maxJumpHoldTime = 1f;
+    [SerializeField] private int maxJumps = 2;
 
-    private Vector3 _moveInput = new Vector3(0, 0, 0);
-
-    [SerializeField] private int speed = 20;
-    [SerializeField] private int jumpForce = 10;
-
-    private bool isJumpRequested;
-    private bool isJumpPressed = false;
-
+    private Vector3 _moveInput = Vector3.zero;
+    private bool isJumpRequested = false;
+    private bool isJumpHeld = false;
+    private bool isJumping = false;
+    private float jumpTimer = 0.5f;
     private int jumps = 0;
-    private int maxJumps = 2;
-    private int maxJumpMultiplier = 2;
 
-
-    /*public void Update()
-    {
-        player.GetComponent<Transform>().position += _moveInput;
-    }
-    */
     public void FixedUpdate()
     {
         rigidBody.AddForce(_moveInput * speed, ForceMode.Force);
 
-        if (isJumpRequested && jumps <= maxJumps)
+        if (jumps <= maxJumps)
         {
-            if (isJumpPressed)
+            if (isJumpRequested)
             {
-
+                rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isJumping = true;
+                jumpTimer = 0f;
+                isJumpRequested = false;
             }
-            isJumpRequested = false;
-            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            if (isJumping && isJumpHeld && jumpTimer < maxJumpHoldTime)
+            {
+                jumpTimer += Time.fixedDeltaTime;
+                rigidBody.AddForce(Vector3.up * holdJumpForce, ForceMode.Force);
+            }
+
+            if (!isJumpHeld || jumpTimer >= maxJumpHoldTime)
+            {
+                isJumping = false;
+            }
         }
     }
 
     public void OnPlayerCollision()
     {
         jumps = 0;
-        Debug.Log("toca el piso");
     }
 
     private void OnEnable()
@@ -55,22 +60,36 @@ public class InputScript : MonoBehaviour
         moveAction.action.performed += HandleMoveInput;
         moveAction.action.canceled += HandleMoveInput;
 
-        jumpAction.action.started += HandleJumpInput;
+        jumpAction.action.started += HandleJumpStarted;
+        jumpAction.action.performed += HandleJumpHeld;
+        jumpAction.action.canceled += HandleJumpReleased;
     }
 
-    private void HandleJumpInput(InputAction.CallbackContext ctx)
+    private void HandleJumpStarted(InputAction.CallbackContext ctx)
     {
-        isJumpRequested = true;
-        jumps++;
-        Debug.Log("Jump " + jumps);
+        if (jumps < maxJumps)
+        {
+            isJumpRequested = true;
+            isJumpHeld = true;
+            jumpTimer = 0f;
+            jumps++;
+        }
+    }
+
+    private void HandleJumpHeld(InputAction.CallbackContext ctx)
+    {
+        isJumpHeld = true;
+    }
+
+    private void HandleJumpReleased(InputAction.CallbackContext ctx)
+    {
+        isJumpHeld = false;
     }
 
     private void HandleMoveInput(InputAction.CallbackContext ctx)
     {
-        _moveInput.x = ctx.ReadValue<Vector2>().x;
-        _moveInput.z = ctx.ReadValue<Vector2>().y;
-
-        Debug.Log(ctx.ReadValue<Vector2>());
+        Vector2 input = ctx.ReadValue<Vector2>();
+        _moveInput.x = input.x;
+        _moveInput.z = input.y;
     }
-
 }
